f55a175634f36737d7fe2b758ec78c7e90b57dad
nksteven <yangshaoli2005@gmail.com>
2019-06-22 07:49:32 +0800

    OC

--- ios/Classes/GetIpPlugin.m
+++ ios/Classes/GetIpPlugin.m
@@ -1,8 +1,51 @@
 #import "GetIpPlugin.h"
-#import <get_ip/get_ip-Swift.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
 @implementation GetIpPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftGetIpPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel =
+    [FlutterMethodChannel methodChannelWithName:@"get_ip"
+                                binaryMessenger:[registrar messenger]];
+    GetIosDeviceIdfaPlugin* instance = [[GetIosDeviceIdfaPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    result([self getIPAddress]);
+}
+
+// Get the INTERNAL ip address
+
+- (NSString *)getIPAddress {
+    
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    
+                }
+                
+            }
+            
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+    
+}
+
 @end
